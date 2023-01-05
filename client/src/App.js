451c2c2fe84317a67856836113959a2ea7eda772
aaryansr26 <aaryansr26@gmail.com>
2023-01-05 18:28:23 +0530

    boiler plate code

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+const App = () => (
+    <Routes>
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element= {<Chat />} />
+    </Routes>
+    
+);
+
+
+export default App;
