c451df1eeabd863dbcceb6dac9586fe80f64a113
aaryansr26 <aaryansr26@gmail.com>
2023-01-06 12:06:41 +0530

    Logic for Joining a Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Join from "./components/Join";
-import Chat from "./components/Chat";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 const App = () => (
     <Routes>
