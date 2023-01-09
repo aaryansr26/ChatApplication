98580fbedb11bb1517d3179e44ef14aceed32405
aaryansr26 <aaryansr26@gmail.com>
2023-01-09 14:57:28 +0530

    dynamic room functionality

--- client/src/components/Infobar/Infobar.js
+++ client/src/components/Infobar/Infobar.js
@@ -1,20 +1,20 @@
 import React from "react";
 import './Infobar.css'
 
-import onlineIcon from '../icons/closeIcon.png'
-import closeIcon from '../icons/onlineIcon'
+import onlineIcon from '../icons/onlineIcon.png'
+import closeIcon from '../icons/closeIcon.png'
 
-const Infobar = () => {
+const Infobar = ({room}) => (
     <div className="infoBar">
         <div className="leftInnerContainer">
             <img className="onlineIcon" src={onlineIcon} alt="onlineIcon"/>
-            <h3>roomName</h3>
+            <h3>{room}</h3>
         </div>
         <div className="rightInnerContainer">
         <a href="/"><img src={closeIcon} alt="closeImage" /></a>
         </div>
     </div>
 
-}
+)
 
 export default Infobar;  
