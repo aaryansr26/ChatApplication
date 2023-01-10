a72134810cdefae39bc207ebbd54c018ac683cd2
aaryansr26 <aaryansr26@gmail.com>
2023-01-11 00:52:10 +0530

    link css file

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import onlineIcon from "../icons/onlineIcon.png"
+import "./TextContainer.css"
 
 const TextContainer = ({users}) => (
     <div className="textContainer">
