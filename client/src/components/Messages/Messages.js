a969f4953a5770d4390c5ffdb0b1181685d4f59b
aaryansr26 <aaryansr26@gmail.com>
2023-01-10 22:15:48 +0530

    added functionality

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -5,10 +5,10 @@ import Message from "../Message/Message.js"
 import "./Messages.css"; 
 
 const Messages = ({messages, name}) => (
-    <ScrollToBottom>
+    <ScrollToBottom className="messages">
         {messages.map((message, i) => <div key={i}><Message message = {message} name={name} /></div>)}
     </ScrollToBottom>
 )
 
-    
+
 export default Messages;
